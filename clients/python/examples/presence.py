6596c9ecc154407e1deed6e96eecc52c26126bbc
Andrew Sorensen <andrew@andrewsorensen.net>
2019-10-23 22:25:11 -0700

    Add sample presence sensor

---
+++ clients/python/examples/presence.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import logging
+
+from ipcdclient.client import IpcdClient
+
+
+def main():
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.DEBUG)
+  ch = logging.StreamHandler()
+  ch.setLevel(logging.DEBUG)
+  logger.addHandler(ch)
+
+  client = IpcdClient('wss://arcus.example.com')
+  device = IpcdClient.Device('Generic', 'PresenceSensor', '12345678')
+
+  client.add_device(device)
+
+  client.connect()
+
+  logger.info("marking device as present")
+
+  client.send(device, [{
+    'parameter': 'generic.presence', 'value': 'present'
+  }])
+
+  client.send(device, [{
+    'parameter': 'generic.presence', 'value': 'absent'
+  }])
+
+  client.disconnect()
+
+if __name__ == "__main__":
+  main()
